f0c851ca5c64da49bf9349f18ce13d708a375624
Mikko Siukola <mikko.siukola@cybercom.com>
2024-09-25 14:39:16 +0300

    OK-590 Add simple tests for findByTimeRange and prosessiRepository

--- ohjausparametrit/src/test/java/fi/oph/ohjausparametrit/repository/ParameterRepositoryTest.java
+++ ohjausparametrit/src/test/java/fi/oph/ohjausparametrit/repository/ParameterRepositoryTest.java
@@ -16,6 +16,8 @@ package fi.oph.ohjausparametrit.repository;
 
 import fi.oph.ohjausparametrit.TestApplication;
 import fi.oph.ohjausparametrit.model.JSONParameter;
+import java.time.OffsetDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -25,6 +27,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -70,6 +73,32 @@ public class ParameterRepositoryTest {
       List<JSONParameter> ps =
           StreamSupport.stream(dao.findAll().spliterator(), false).collect(Collectors.toList());
       Assert.assertEquals("Param repo should be empty", ps.size(), 0);
+    }
+  }
+
+  @Test
+  public void testFindByTimeRange() {
+    {
+      JSONParameter p = new JSONParameter();
+      p.setTarget("A");
+      p.setJsonValue("{ \"foo\": false}");
+      p.setMuokkaaja("Frank");
+      p.setMuokattu(new Date());
+      dao.save(p);
+
+      p = new JSONParameter();
+      p.setTarget("B");
+      p.setJsonValue("{ \"foo\": true}");
+      p.setMuokkaaja("Frank Armoton");
+      p.setMuokattu(new Date());
+      dao.save(p);
+
+      OffsetDateTime nyt = OffsetDateTime.now();
+      OffsetDateTime eilen = nyt.minusDays(1);
+      List<JSONParameter> results =
+          dao.findByTimeRange(
+              Date.from(eilen.toInstant()), Date.from(nyt.toInstant()), PageRequest.of(0, 5000));
+      Assert.assertEquals("kaikki löytyy", 2, results.size());
     }
   }
 }
